d7bdb79a85a3f7be295d5098ba212b07456c1bc0
GODLESSishere <eba0bbobagmail.com@>
2024-11-21 21:26:40 +0300

    so hard

---
+++ server/docs/html/search/all_d.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['showhelp_0',['showHelp',['../classInterface.html#a4d4abe80e544fb1af93fa6f1087a2848',1,'Interface']]],
+  ['socket_1',['socket',['../classNetwork.html#a581d7031a09e7bf6715b6ee010546e74',1,'Network']]]
+];
---
+++ server/docs/html/search/searchdata.js
@@ -0,0 +1,30 @@
+var indexSectionsWithContent =
+{
+  0: "acdefgilmnpqrsw",
+  1: "adein",
+  2: "c",
+  3: "ceimn",
+  4: "acdegilmnpqrsw",
+  5: "acdflmnps"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "variables"
+};
+
+var indexSectionLabels =
+{
+  0: "Указатель",
+  1: "Классы",
+  2: "Пространства имен",
+  3: "Файлы",
+  4: "Функции",
+  5: "Переменные"
+};
+
